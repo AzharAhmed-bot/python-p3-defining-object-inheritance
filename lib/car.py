2b802eacd0229c6b66bbf3218179d56fbea68f43
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-05-18 17:39:38 +0300

    Complete assignment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,11 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
     pass
+  
+print(Car.__base__)
+print(type(Car))
+car2=Car(20,4)
+print(car2.go())
